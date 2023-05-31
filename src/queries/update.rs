86c31158cd38c11b88adb67e9eab774154004dc4
Aelto <Aelto@users.noreply.github.com>
2023-05-31 17:19:55 +0200

    add examples

--- src/queries/update.rs
+++ src/queries/update.rs
@@ -5,6 +5,18 @@ use super::query;
 use super::BindingMap;
 use super::QueryBuilderInjecter;
 
+/// # Example
+/// ```rs
+/// use crate::prelude::*;
+/// use serde_json::Value;
+///
+/// let filter = Set(serde_json::json!({ "name": "John", "age": 10 }));
+/// let (query, params) = update("User", filter).unwrap();
+///
+/// assert_eq!("UPDATE User SET age = $age , name = $name", query);
+/// assert_eq!(params.get("name"), Some(&Value::from("John".to_owned())));
+/// assert_eq!(params.get("age"), Some(&Value::from(10)));
+/// ```
 pub fn update<'a, 'b>(
   table: &'a str, component: impl QueryBuilderInjecter<'a> + 'a,
 ) -> serde_json::Result<(String, BindingMap)> {
--- src/types/set.rs
+++ src/types/set.rs
@@ -2,6 +2,15 @@ use crate::prelude::QueryBuilder;
 use crate::prelude::QueryBuilderInjecter;
 use crate::queries::BindingMap;
 
+/// # Example
+/// ```rs
+/// let filter = Set(serde_json::json!({ "name": "John", "age": 10 }));
+/// let (query, params) = update("User", filter).unwrap();
+///
+/// assert_eq!("UPDATE User SET age = $age , name = $name", query);
+/// assert_eq!(params.get("name"), Some(&Value::from("John".to_owned())));
+/// assert_eq!(params.get("age"), Some(&Value::from(10)));
+/// ```
 pub struct Set<T>(pub T);
 
 impl<'a, T: QueryBuilderInjecter<'a>> QueryBuilderInjecter<'a> for Set<T> {
