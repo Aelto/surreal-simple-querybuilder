1ebf78a7d1584838e47e22c9ea885ecbeaad8e1b
Aelto <Aelto@users.noreply.github.com>
2023-06-06 12:23:07 +0200

    add doc, add function to ease pagination construction

--- src/types/pagination.rs
+++ src/types/pagination.rs
@@ -3,6 +3,12 @@ use std::ops::Range;
 use crate::prelude::QueryBuilder;
 use crate::prelude::QueryBuilderInjecter;
 
+/// Declare a LIMIT and START AT clause that will include the items from the
+/// supplied range. The [`Self::new_page(page: u64, page_size: u64)`] function
+/// offers an easy way to construct a range with a given page & page size.
+///
+/// _The START AT clause is omitted if the left side of the range is lower or
+/// equal than 0._
 pub struct Pagination(pub Range<u64>);
 
 impl From<Range<u64>> for Pagination {
@@ -14,6 +20,14 @@ impl From<Range<u64>> for Pagination {
 impl Pagination {
   pub fn new(range: Range<u64>) -> Self {
     Pagination(range)
+  }
+
+  /// Create a new [Pagination] for the given `page` where each page contains
+  /// `page_size` elements. This function assumes the pagination starts at page
+  /// 0, so `Pagination::new_page(1, 20)` is the second page with elements from
+  /// the `20..40` range.
+  pub fn new_page(page: u64, page_size: u64) -> Self {
+    Self::new(page * 20..(page + 1) * 20)
   }
 
   pub fn limit(&self) -> u64 {
