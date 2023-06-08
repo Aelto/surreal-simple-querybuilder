3b3d6b145e7a3f267578f9b0508ec5880d0b7965
Aelto <Aelto@users.noreply.github.com>
2023-06-08 12:03:31 +0200

    grammar

--- src/types/build.rs
+++ src/types/build.rs
@@ -2,8 +2,8 @@ use crate::prelude::QueryBuilder;
 use crate::prelude::QueryBuilderInjecter;
 
 /// An unintuitive way to access the underlying querybuilder that is passed to
-/// the querybuilder injecters. It is considered unintuitive because if you
-/// access to the raw querybuilder in injecters then you may as well just
+/// the querybuilder injecters. It is considered unintuitive because if you want
+/// an access to the raw querybuilder while  in injecters then you may as well just
 /// use the querybuilder without the injecters, but there are special cases where
 /// you may want to use injecters everywhere (which is valid) and this `Build`
 /// type allows you to play around the current limitations of the injecters without
