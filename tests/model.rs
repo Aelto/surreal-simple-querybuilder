bbc7e0bed3c59b1c90bba34a95cf9e7512f68ec0
Aelto <Aelto@users.noreply.github.com>
2024-02-22 16:58:16 +0100

    fixes #11 update parser to use complex Identifier type

---
+++ tests/model.rs
@@ -0,0 +1,32 @@
+#![allow(incomplete_features)]
+#![feature(generic_const_exprs)]
+
+#[cfg(feature = "querybuilder")]
+#[cfg(feature = "queries")]
+#[cfg(feature = "model")]
+#[cfg(feature = "foreign")]
+
+mod one {
+  use serde::Serialize;
+
+  surreal_simple_querybuilder::model!(TestModel0 as model_base with(partial) {
+    id,
+  });
+}
+
+mod two {
+  use super::one::model_base::TestModel0;
+
+  surreal_simple_querybuilder::model!(TestModel1 {
+    id,
+    pub r#in,
+    pub other<TestModel0>,
+    pub ->relation->TestModel0 as r#for
+  });
+
+  #[test]
+  fn test_string_literal() {
+    assert_eq!(schema::model.r#in.to_string(), "in");
+    assert_eq!(schema::model.r#for.to_string(), "->relation->TestModel0");
+  }
+}
